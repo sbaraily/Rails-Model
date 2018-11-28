fd8ba2eb9e83bf4f4d0b22f8778e42900a04834f
Samir Baraily <samir.baraily@gmail.com>
2018-11-27 21:13:33 -0700

    Edited Model and Controller

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,13 @@
+class PeopleController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'people/index'
+  get 'people/show'
+  get 'people/new'
+  get 'people/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
