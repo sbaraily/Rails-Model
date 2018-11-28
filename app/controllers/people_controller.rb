287fcaf48e6093018f27339a62e946e791a49cf9
Samir Baraily <samir.baraily@gmail.com>
2018-11-27 22:56:32 -0700

    Did the best I could

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,13 +1,52 @@
 class PeopleController < ApplicationController
+
+  before_action :set_person, only: [:show, :update, :edit, :destroy]
+  
   def index
+    @people = Person.all
   end
 
   def show
   end
 
   def new
+    @person = Person.new
+    render partial: "form"
+  end
+
+  def create
+    @person = Person.new(person_params)
+    if @person.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
+    render partial: "form"
   end
+
+  def update
+    if @person.update(person_params)
+      redirect_to @person
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @person.destroy
+    redirect_to root_path
+  end
+
+
+  private
+    def person_params
+      params.require(:person).permit[:name]
+    end
+
+    def set_person
+      @person = Person.find(params[:id])
+    end
 end
